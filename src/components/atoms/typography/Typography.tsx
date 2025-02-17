b474ef625acf99a59b37ca5934e5943f999d01a2
frnlb <franlobogarcia@gmail.com>
2025-02-17 19:28:27 +0100

    draft: add some basic typography & header folder

---
+++ src/components/atoms/typography/Typography.tsx
@@ -0,0 +1,14 @@
+import { TypographyTag } from "@/interfaces/common";
+
+export interface TypographyProps {
+    variant: "title" | "normal";
+    color: "black" | "gray";
+}
+
+export const Typography = () => {
+    const Tag = ;
+
+    return (
+        <Tag>Typography</Tag>
+    )
+}
---
+++ src/interfaces/common.ts
@@ -0,0 +1,1 @@
+export type TypographyTag = "h3" | "p";
