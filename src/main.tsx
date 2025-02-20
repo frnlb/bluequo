b81161d549bb581bcf2be059bdb8609291307412
frnlb <franlobogarcia@gmail.com>
2025-02-20 20:08:25 +0100

    feat: add apollo client and wrap app with apollo provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloProvider } from "@apollo/client";
+import App from "./App";
+import client from "@/lib/graphql/apollo-client";
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+       <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
