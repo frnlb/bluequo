4f28a43e7452634b2d9e8a738fef584e785615b3
frnlb <franlobogarcia@gmail.com>
2025-02-21 00:57:01 +0100

    feat: add search input & remove unused file & update button component

--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./button.module.css";
 export interface ButtonProps {
     children: React.ReactNode | React.ReactNode[];
-    onClick: () => void;
-
+    onClick?: () => void;
+    type?: "button" | "submit" | "reset";
 }
 
-export const Button = ({children, onClick}: ButtonProps) => {
+export const Button = ({children, onClick, type="button"}: ButtonProps) => {
   return (
     <button 
+        type={type}
         onClick={onClick}
         className={styles.wrapper}>
         {children}
---
+++ src/components/molecules/input/SearchInput.tsx
@@ -0,0 +1,32 @@
+import { useRef } from 'react';
+import { Button, Icon } from '@/components';
+import styles from './search-input.module.css';
+
+interface SearchInputProps {
+  handleSearch: (searchTerm: string) => void;
+  placeholder?: string;
+}
+
+export const SearchInput: React.FC<SearchInputProps> = ({ handleSearch, placeholder = "You're looking for something?" }) => {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (searchInputRef.current) {
+      handleSearch(searchInputRef.current.value);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit} className={styles.wrapper}>
+      <Button type="submit"><Icon name="magnifying-glass" color="inherit"/></Button>
+      <input 
+        type="search" 
+        ref={searchInputRef} 
+        placeholder={placeholder} 
+        className={styles.searchBar}
+      />
+      
+    </form>
+  );
+};
