3b203d4bfc0c677a10f706a934c660937aba39a2
frnlb <franlobogarcia@gmail.com>
2025-02-20 20:10:51 +0100

    feat: add CardData interface

--- src/interfaces/common.ts
+++ src/interfaces/common.ts
@@ -1,1 +1,22 @@
 export type TypographyTag = "h3" | "p" | "span";
+
+export interface CardData {
+    author: string;
+    createdAt: string;
+    id: string;
+    liked: boolean;
+    likesCount: number;
+    picture: string;
+    price: number;
+    title: string;
+    updatedAt: string;
+}
+
+export interface ImageConnection {
+    images: {
+      edges: {
+        node: CardData;
+      }[];
+    };
+  }
+  
